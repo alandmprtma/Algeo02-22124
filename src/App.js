c1c4d413728e376eced820542645893a588586b3
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2023-11-09 09:59:31 +0700

    modified file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import ImageHandler from './Feature/ImageHandler.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Cbir from './components/Cbir.jsx';
+import Home from './components/Home.jsx';
+import Aboutus from './components/Aboutus.jsx';
 
 function App() {
   return (
-    <div className="">
-      <ImageHandler/>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='' element ={<Home/>}/>
+        <Route path='/CBIR' element ={<Cbir/>}/>
+        <Route path='/about-us' element ={<Aboutus/>}/>
+      </Routes>
+    </Router>
   );
 }
 
