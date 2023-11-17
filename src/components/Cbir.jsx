7562ec1f1f0fd281be85b02fb6539d90a90c71bc
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2023-11-18 02:09:18 +0700

    feat: styling CBIR & fix pagination

--- src/components/Cbir.jsx
+++ src/components/Cbir.jsx
@@ -17,6 +17,7 @@ import ImageScraper from './ImageScraper'
 const Cbir = () => {
   const [uploadMode, setUploadMode] = useState('image'); // 'image' or 'camera'
   const [datasetMode, setDatasetMode] = useState('dataset'); // 'dataset' or 'scrapper'
+  const [resultMode, setResultMode] = useState('show'); // 'show' or 'hide'
   const [uploadedImage, setUploadedImage] = useState(null);
   const [uploadDataset, setUploadDataset] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
@@ -66,6 +67,10 @@ const Cbir = () => {
     setDatasetMode((prevMode) => (prevMode === 'dataset' ? 'scrapper' : 'dataset'));
   };
 
+  const toggleResultMode = () => {
+    setResultMode((prevMode) => (prevMode === 'show' ? 'hide' : 'show'));
+  };
+
 
   const handleImageUpload = (image) => {
     // Logic to handle image upload and set it in state
@@ -83,15 +88,17 @@ const Cbir = () => {
       <img src={Bg} className='w-full h-full top-0 fixed left-0 z-[-100] object-cover'/>
       <img src={logo} className='h-[150px] object-cover'/>    
       <h1 className='font-inter-bold text-4xl text-white mt-6'> Reverse Image Search </h1>
-      <article className='flex w-[80%] gap-x-[100px] justify-center mt-10'>
-      <div className='rounded my-2 w-[550px] h-[350px] relative 
+      <article className='flex w-[80%] gap-x-[100px] justify-center mt-10 items-center'>
+      <div>
+      <p className='font-inter-bold mt-7 text-2xl text-white text-left'>Image Preview</p>
+      <div className='rounded my-2 w-[600px] h-[500px] relative 
           before:content-[""] before:absolute before:top-0 before:left-0 before:h-[100%] before:w-[100%] before:bg-gradient before:-z-1 before:rounded
           after:content-[""] after:absolute after:top-0 after:left-0 after:h-[100%] after:w-[100%] after:bg-gradient after:blur-[10px] after:-z-1 flex items-center justify-center after:rounded'>
             <div className='relative z-10 bg-primary rounded w-[99%] h-[99%] p-4 flex justify-center items-center text-white'>
             <img src={upload || icon} className='h-full w-full object-cover'/>  
             </div>
           </div>
-          
+      </div>
       <div className ='w-[400px]'>
        <p className='font-inter-bold mt-7 text-2xl text-white text-left'>Image Input</p>
        <div className='flex flex-col items-center'>
@@ -128,14 +135,16 @@ const Cbir = () => {
           before:content-[""] before:absolute before:top-0 before:left-0 before:h-[100%] before:w-[100%] before:bg-gradient before:-z-1 before:rounded-[20px]
           after:content-[""] after:absolute after:top-0 after:left-0 after:h-[100%] after:w-[100%] after:bg-gradient after:blur-[20px] after:-z-1 flex items-center justify-center after:rounded-[15px]'
         >
-          <button className='font-inter-bold text-xl text-white z-10 hover:scale-105 cursor-pointer transition-all' onClick={searchHandler}>Show Result</button>
+          <button className={`font-inter-bold text-xl hover:scale-105 cursor-pointer transition-all text-white z-10 ${ resultMode === 'show'}`}   onClick={() => {toggleResultMode(); searchHandler();}}> 
+        {resultMode === 'show' ? 'Hide Result' : 'Show Result'}
+      </button>
         </div>
         </div>
       </div>
       </article>
       <article className='w-[80%] flex flex-col justify-center items-center'>
        <div className="bg-white h-[2px] w-full"/>
-       <div className=" flex flex-col relative h-[950px] w-full justify-between">
+       <div className=" flex flex-col relative h-fit w-full justify-between">
         <div className='flex justify-between items-center mt-4'>
         <div className='flex items-start h-[25px]'>
         <h2 className='font-inter-bold text-xl text-white h-fit'> Result : </h2>
